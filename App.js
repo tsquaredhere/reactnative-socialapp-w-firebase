4b184a8df47db2d78a7160ea0bb4bc4ae6389e24
tsquaredhere <tsquaredank@gmail.com>
2020-01-15 12:53:32 -0800

    firebase connection, home, loading, login, register screens

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNvaigator} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import LoadingScreen from './screens/LoadingScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import HomeScreen from './screens/HomeScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import * as firebase from 'firebase'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCQJeq2jIuGJKisnkUO8SKpJdBlZBviDIo",
+  authDomain: "socialapp-5f0b3.firebaseapp.com",
+  databaseURL: "https://socialapp-5f0b3.firebaseio.com",
+  projectId: "socialapp-5f0b3",
+  storageBucket: "socialapp-5f0b3.appspot.com",
+  messagingSenderId: "178201199967",
+  appId: "1:178201199967:web:406a97b89399427f0062ea"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const APpStack = createStackNvigator({
+  Home: HomeScreen
+})
+
+const AppStack = createStackNavigator({
+  Home: HomeScreen
 });
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
+});
+
+export default createAppContainer(
+  createSwitchNavigator(
+      {
+          Loading: LoadingScreen,
+          App: AppStack,
+          Auth: AuthStack
+      },
+      {
+          initialRouteName: "Loading"
+      }
+  )
+);
