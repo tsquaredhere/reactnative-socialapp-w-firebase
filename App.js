832f96d608ab97ec21392d34de3fe8e50c16bdb5
tsquaredhere <tsquaredank@gmail.com>
2020-01-15 16:07:39 -0800

    package versions fixed

--- App.js
+++ App.js
@@ -1,46 +1,93 @@
-import {createAppContainer, createSwitchNvaigator} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
-import LoadingScreen from './screens/LoadingScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import HomeScreen from './screens/HomeScreen'
+import React from "react";
+import FirebaseKeys from "./config";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
 
-import * as firebase from 'firebase'
+import HomeScreen from "./screens/HomeScreen";
+import MessageScreen from "./screens/MessageScreen";
+import PostScreen from "./screens/PostScreen";
+import NotificationScreen from "./screens/NotificationScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
+import * as firebase from "firebase";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCQJeq2jIuGJKisnkUO8SKpJdBlZBviDIo",
-  authDomain: "socialapp-5f0b3.firebaseapp.com",
-  databaseURL: "https://socialapp-5f0b3.firebaseio.com",
-  projectId: "socialapp-5f0b3",
-  storageBucket: "socialapp-5f0b3.appspot.com",
-  messagingSenderId: "178201199967",
-  appId: "1:178201199967:web:406a97b89399427f0062ea"
-};
-// Initialize Firebase
+const firebaseConfig = FirebaseKeys;
+
 firebase.initializeApp(firebaseConfig);
-const APpStack = createStackNvigator({
-  Home: HomeScreen
-})
 
-const AppStack = createStackNavigator({
-  Home: HomeScreen
-});
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={24} color={tintColor} />
+            }
+        },
+        Message: {
+            screen: MessageScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-chatboxes" size={24} color={tintColor} />
+            }
+        },
+        Post: {
+            screen: PostScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Ionicons
+                        name="ios-add-circle"
+                        size={48}
+                        color="#E9446A"
+                        style={{
+                            shadowColor: "#E9446A",
+                            shadowOffset: { width: 0, height: 10 },
+                            shadowRadius: 10,
+                            shadowOpacity: 0.3
+                        }}
+                    />
+                )
+            }
+        },
+        Notification: {
+            screen: NotificationScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-notifications" size={24} color={tintColor} />
+            }
+        },
+        Profile: {
+            screen: ProfileScreen,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => <Ionicons name="ios-person" size={24} color={tintColor} />
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: "#161F3D",
+            inactiveTintColor: "#B8BBC4",
+            showLabel: false
+        }
+    }
+);
 
 const AuthStack = createStackNavigator({
-  Login: LoginScreen,
-  Register: RegisterScreen
+    Login: LoginScreen,
+    Register: RegisterScreen
 });
 
 export default createAppContainer(
-  createSwitchNavigator(
-      {
-          Loading: LoadingScreen,
-          App: AppStack,
-          Auth: AuthStack
-      },
-      {
-          initialRouteName: "Loading"
-      }
-  )
+    createSwitchNavigator(
+        {
+            Loading: LoadingScreen,
+            App: AppTabNavigator,
+            Auth: AuthStack
+        },
+        {
+            initialRouteName: "Loading"
+        }
+    )
 );
---
+++ config.js
@@ -0,0 +1,9 @@
+export default FirebaseKeys = {
+    apiKey: "AIzaSyCQJeq2jIuGJKisnkUO8SKpJdBlZBviDIo",
+    authDomain: "socialapp-5f0b3.firebaseapp.com",
+    databaseURL: "https://socialapp-5f0b3.firebaseio.com",
+    projectId: "socialapp-5f0b3",
+    storageBucket: "socialapp-5f0b3.appspot.com",
+    messagingSenderId: "178201199967",
+    appId: "1:178201199967:web:406a97b89399427f0062ea"
+  }
